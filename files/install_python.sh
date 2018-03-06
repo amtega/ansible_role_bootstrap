a79dc699b5cd55ca2b97154fc9aee0ffe32f89a9
Juan Antonio Valino Garcia <juanval@edu.xunta.es>
2018-03-06 13:25:51 +0100

    Improved python install script.

--- files/install_python.sh
+++ files/install_python.sh
@@ -3,11 +3,9 @@
 changed=0
 
 if [ -f /usr/bin/dnf ]; then
-  rpm -q python2 || ( dnf install -y python2 ; changed=1)
+  rpm -q python2 || ( dnf install -y python2 ; exit 1)
 elif [ -f /usr/bin/yum ]; then
-  rpm -q python2 || ( yum install -y python2 ; changed=1)
+  rpm -q python2 || ( yum install -y python2 ; exit 1)
 else
-  changed=-1
+  exit -1
 fi
-
-exit $changed
