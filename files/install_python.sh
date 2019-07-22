78961762e61f9e194ff82f292279fd32120d2fad
Juan Antonio Valino Garcia <juanval@edu.xunta.es>
2019-07-22 14:28:04 +0200

    Fixed bug.

--- files/install_python.sh
+++ files/install_python.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f /usr/bin/dnf ]; then
-  (rpm -q python3 && rpm -q python3-virtualenv) || (dnf install -y python3 python3-virtualenv ; exit 1)
+  (rpm -q python3 && rpm -q python3-virtualenv) || (dnf install -y python3 python3-virtualenv ; exit 1) || exit 2
 elif [ -f /usr/bin/yum ]; then
-  (rpm -q python && rpm -q python-virtualenv) || (yum install -y python python-virtualenv ; exit 1)
+  (rpm -q python && rpm -q python-virtualenv) || (yum install -y python python-virtualenv ; exit 1) || exit 2
 else
-  exit 1
+  exit 2
 fi
